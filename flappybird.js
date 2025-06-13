6d235c6cf0d971c7ad2bd524b8c119a06ada9cc0
Narayanan Subramanian <csnarayanan2006@gmail.com>
2025-06-13 10:12:21 +0530

    Update flappybird.js

--- flappybird.js
+++ flappybird.js
@@ -1,56 +1,28 @@
+// Get the canvas element from the HTML file
 const canvas = document.getElementById("board");
 const ctx = canvas.getContext("2d");
 
+// Set the game's dimensions
 const boardWidth = 360;
 const boardHeight = 640;
-
-// Set the canvas dimensions in JavaScript
 canvas.width = boardWidth;
 canvas.height = boardHeight;
 
-// Load images
+// Create Image objects to hold our pictures
 const backgroundImg = new Image();
-backgroundImg.src = "./flappybirdbg.png";
-
 const birdImg = new Image();
-birdImg.src = "./flappybird.png";
-
 const topPipeImg = new Image();
-topPipeImg.src = "./toppipe.png";
-
 const bottomPipeImg = new Image();
-bottomPipeImg.src = "./bottompipe.png";
 
-// Bird setup
-const bird = {
-    x: boardWidth / 8,
-    y: boardHeight / 2,
-    width: 34,
-    height: 24,
-    img: birdImg
-};
+// This is the main starting point of our game.
+// It will run only after the entire HTML page has finished loading.
+window.onload = function() {
+    // Tell the browser which image files to load
+    backgroundImg.src = "./flappybirdbg.png";
+    birdImg.src = "./flappybird.png";
+    topPipeImg.src = "./toppipe.png";
+    bottomPipeImg.src = "./bottompipe.png";
 
-// Pipe setup
-const pipe = {
-    x: boardWidth,
-    y: 0,
-    width: 64,
-    height: 512,
-    topImg: topPipeImg,
-    bottomImg: bottomPipeImg
-};
-
-// Draw initial setup only after the main background image has loaded
-backgroundImg.onload = function() {
-    draw();
-};
-
-function draw() {
-    ctx.drawImage(backgroundImg, 0, 0, boardWidth, boardHeight);
-    ctx.drawImage(bird.img, bird.x, bird.y, bird.width, bird.height);
-    
-    // Example pipe drawing
-    let openingSpace = 120; // The space between the top and bottom pipe
-    ctx.drawImage(pipe.topImg, pipe.x, pipe.y, pipe.width, pipe.height);
-    ctx.drawImage(pipe.bottomImg, pipe.x, pipe.y + pipe.height + openingSpace, pipe.width, pipe.height);
+    // Start the game's animation loop. The 'update' function will now run continuously.
+    requestAnimationFrame(update);
 }
