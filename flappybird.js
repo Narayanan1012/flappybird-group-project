1ed38494f2fd69597f53160cfc4da68f6d8907b2
Narayanan Subramanian <csnarayanan2006@gmail.com>
2025-06-13 11:16:01 +0530

    Update flappybird.js

--- flappybird.js
+++ flappybird.js
@@ -7,6 +7,23 @@ const boardWidth = 360;
 const boardHeight = 640;
 canvas.width = boardWidth;
 canvas.height = boardHeight;
+//bird
+let birdHeight=24;
+let birdWidth=34;
+let birdX=boardwidth/8;
+let birdY=boardHeight/2;
+let bird = {
+    x : birdX,
+    y : birdY,
+    width : birdWidth,
+    height  : birdHeight
+}
+//pipes
+let pipeArray = [];
+let pipeWidth = 64;
+let pipeHeight = 512;
+let pipeX = boardwidth;
+let pipeY = 0;
 
 // Create Image objects to hold our pictures
 const backgroundImg = new Image();
@@ -25,4 +42,6 @@ window.onload = function() {
 
     // Start the game's animation loop. The 'update' function will now run continuously.
     requestAnimationFrame(update);
+    setInterval(placePipes,1500);
+    
 }
