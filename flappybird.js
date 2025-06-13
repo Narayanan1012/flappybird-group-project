a46a0e64ec4578aa23b3c509eeb861649a529484
Narayanan Subramanian <csnarayanan2006@gmail.com>
2025-06-13 08:48:44 +0530

    Update flappybird.js

--- flappybird.js
+++ flappybird.js
@@ -1,69 +1,56 @@
+const canvas = document.getElementById("gameCanvas");
+const ctx = canvas.getContext("2d");
 
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Flappy Bird Setup</title>
-    <style>
-        canvas {
-            background: #70c5ce;
-            display: block;
-            margin: 0 auto;
-        }
-    </style>
-</head>
-<body>
-    <canvas id="gameCanvas" width="360" height="640"></canvas>
+const boardWidth = 360;
+const boardHeight = 640;
 
-    <script>
-        const canvas = document.getElementById("gameCanvas");
-        const ctx = canvas.getContext("2d");
+// Set the canvas dimensions in JavaScript
+canvas.width = boardWidth;
+canvas.height = boardHeight;
 
-        const boardWidth = canvas.width;
-        const boardHeight = canvas.height;
+// Load images
+const backgroundImg = new Image();
+backgroundImg.src = "./flappybirdbg.png";
 
-        // Load images
-        const backgroundImg = new Image();
-        backgroundImg.src = "./flappybirdbg.png";
+const birdImg = new Image();
+birdImg.src = "./flappybird.png";
 
-        const birdImg = new Image();
-        birdImg.src = "./flappybird.png";
+const topPipeImg = new Image();
+topPipeImg.src = "./toppipe.png";
 
-        const topPipeImg = new Image();
-        topPipeImg.src = "./toppipe.png";
+const bottomPipeImg = new Image();
+bottomPipeImg.src = "./bottompipe.png";
 
-        const bottomPipeImg = new Image();
-        bottomPipeImg.src = "./bottompipe.png";
+// Bird setup
+const bird = {
+    x: boardWidth / 8,
+    y: boardHeight / 2,
+    width: 34,
+    height: 24,
+    img: birdImg
+};
 
-        // Bird setup
-        const bird = {
-            x: boardWidth / 8,
-            y: boardHeight / 2,
-            width: 34,
-            height: 24,
-            img: birdImg
-        };
+// Pipe setup
+const pipe = {
+    x: boardWidth,
+    y: 0,
+    width: 64,
+    height: 512,
+    topImg: topPipeImg,
+    bottomImg: bottomPipeImg
+};
 
-        // Pipe setup
-        const pipe = {
-            x: boardWidth,
-            y: 0,
-            width: 64,
-            height: 512,
-            topImg: topPipeImg,
-            bottomImg: bottomPipeImg
-        };
+// Draw initial setup only after the main background image has loaded
+backgroundImg.onload = function() {
+    draw();
+};
 
-        // Draw initial setup
-        backgroundImg.onload = function() {
-            draw();
-        };
-
-        function draw() {
-            ctx.drawImage(backgroundImg, 0, 0, boardWidth, boardHeight);
-            ctx.drawImage(bird.img, bird.x, bird.y, bird.width, bird.height);
-            ctx.drawImage(pipe.topImg, pipe.x, pipe.y, pipe.width, pipe.height);
-            ctx.drawImage(pipe.bottomImg, pipe.x, pipe.y + pipe.height + 100, pipe.width, pipe.height);
-        }
-    </script>
-</body>
-</html>
+function draw() {
+    ctx.drawImage(backgroundImg, 0, 0, boardWidth, boardHeight);
+    ctx.drawImage(bird.img, bird.x, bird.y, bird.width, bird.height);
+    
+    // Example pipe drawing
+    let openingSpace = 120; // The space between the top and bottom pipe
+    ctx.drawImage(pipe.topImg, pipe.x, pipe.y, pipe.width, pipe.height);
+    ctx.drawImage(pipe.bottomImg, pipe.x, pipe.y + pipe.height + openingSpace, pipe.width, pipe.height);
+}
