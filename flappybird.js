d910d4ed67ef119be86dd06fb8cb997d4a879a93
Narayanan Subramanian <csnarayanan2006@gmail.com>
2025-06-14 12:02:04 +0530

    Update flappybird.js

--- flappybird.js
+++ flappybird.js
@@ -1,58 +1,53 @@
-// Get the canvas element from the HTML file
-
 let ctx;
 let canvas;
+
 // Set the game's dimensions
 const boardWidth = 360;
 const boardHeight = 640;
 
-//bird
-let birdHeight=24;
-let birdWidth=34;
-let birdX=boardwidth/8;
-let birdY=boardHeight/2;
+// bird
+let birdHeight = 24;
+let birdWidth = 34;
+let birdX = boardWidth / 8;
+let birdY = boardHeight / 2;
+
 let bird = {
-    x : birdX,
-    y : birdY,
-    width : birdWidth,
-    height  : birdHeight
-}
-//pipes
+    x: birdX,
+    y: birdY,
+    width: birdWidth,
+    height: birdHeight
+};
+
+// pipes
 let pipeArray = [];
 let pipeWidth = 64;
 let pipeHeight = 512;
-let pipeX = boardwidth;
+let pipeX = boardWidth;
 let pipeY = 0;
 
-// Create Image objects to hold our pictures
-
+// Images
 const birdImg = new Image();
 const topPipeImg = new Image();
 const bottomPipeImg = new Image();
 
-// This is the main starting point of our game.
-// It will run only after the entire HTML page has finished loading.
-window.onload = function() {
-    // Tell the browser which image files to load
+window.onload = function () {
     canvas = document.getElementById("board");
     ctx = canvas.getContext("2d");
+
     birdImg.src = "./flappybird.png";
     topPipeImg.src = "./toppipe.png";
     bottomPipeImg.src = "./bottompipe.png";
-    birdImg.onload = function(){
-        ctx.drawImage(birdImg,bird.x,bird.y,bird.width,bird.height);
-    }
-    // Start the game's animation loop. The 'update' function will now run continuously.
-    requestAnimationFrame(update);
-    
-}
-function update(){
 
+    birdImg.onload = function () {
+        ctx.drawImage(birdImg, bird.x, bird.y, bird.width, bird.height);
+    };
 
     requestAnimationFrame(update);
+};
 
-    ctx.clearRect(0,0,board.width,board.height);
+function update() {
+    requestAnimationFrame(update);
 
-    
-    ctx.drawImage(birdImg,bird.x,bird.y,bird.width,bird.height);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.drawImage(birdImg, bird.x, bird.y, bird.width, bird.height);
 }
