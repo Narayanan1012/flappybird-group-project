91ef24378e34cb9e105148680e6a4fe77e5cd5cc
Narayanan Subramanian <csnarayanan2006@gmail.com>
2025-06-15 13:02:47 +0530

    Sound js update

--- flappybird.js
+++ flappybird.js
@@ -35,12 +35,19 @@ let pipeSpawnTimer = 0;
 const topPipeImg = new Image();
 const bottomPipeImg = new Image();
 
+//audio
+let wingsound = new Audio("./sfx_wing.wav");
+let pointsound = new Audio("./sfx_point.wav");
+let clashsound = new Audio("./sfx_hit.wav");
+let bgm =  new Audio("./bgm_mario.mp3");
+
 // game
 let gameOver = false;
 let gameStarted = false;
 let score = 0;
 
 window.onload = function () {
+    bgm.loop= true;
     canvas = document.getElementById("board");
     ctx = canvas.getContext("2d");
 
@@ -91,7 +98,7 @@ function update() {
     }
     
     if (gameOver) return;
-   
+    
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     
 
@@ -115,16 +122,21 @@ function update() {
         //Collision Detection
         if (detectCollision(bird, pipe)){
             gameOver = true;
+            clashsound.play();
         }
 
         if(!pipe.passed && pipe.img === bottomPipeImg && pipe.x + pipe.width < bird.x) {
            pipe.passed = true;
            score++;
+           pointsound.currentTime = 0;
+           pointsound.play();
+
         }
     }
     //Game over message
     if(bird.y + bird.height >= boardHeight || bird.y < 0){
         gameOver = true;
+        clashsound.play();
     }
 
     ctx.fillStyle = "white";
@@ -132,6 +144,7 @@ function update() {
     ctx.fillText("Score: " + score, 10, 40);
 
     if(gameOver){
+        bgm.pause();
         ctx.drawImage(birdImgs[birdImgsIndex],bird.x, bird.y, bird.width, bird.height);
         ctx.fillStyle = "red";
         ctx.font = "40px Arial";
@@ -147,15 +160,19 @@ function moveBird(e) {
     if (e.code == "Space" || e.code == "ArrowUp") {
         if(!gameStarted){
             gameStarted = true;
+            bgm.play();
         }
+        wingsound.play();
         velocityY = jumpPower;
     }
 }
 function tmoveBird() {
     if(!gameStarted){
         gameStarted= true;
+        bgm.play()
     }
     velocityY = jumpPower;
+    wingsound.play();
 }
 
 // Create pipe pair
@@ -206,5 +223,6 @@ function restartGame(){
         score = 0;
          gameOver = false;
          gameStarted = false;
+         bgm.currentTime = 0;
 }
     
